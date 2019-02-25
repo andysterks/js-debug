7241ff57cb499dda27481c4b8cea5ebe68550c5e
Andy Sterkowitz <apsterks@gmail.com>
2019-02-24 20:47:00 -0600

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 document.getElementById("add-button").addEventListener("click", addNumbers);
-document.getElementById("subtract-button").addEventListener("click", addNumbers);
-document.getElementById("multiply-button").addEventListener("click", addNumbers);
-document.getElementById("divide-button").addEventListener("click", addNumbers);
 
 function addNumbers() {
     var numberOne = getNumberOne();
@@ -9,30 +6,6 @@ function addNumbers() {
 
     var sum = numberOne + numberTwo;
     document.getElementById("total").innerHTML = 'Total: ' + sum;
-}
-
-function subtractNumbers() {
-    var numberOne = getNumberOne();
-    var numberTwo = getNumberTwo();
-
-    var difference = numberOne - numberTwo;
-    document.getElementById("total").innerHTML = 'Total: ' + difference;
-}
-
-function multiplyNumbers() {
-    var numberOne = getNumberOne();
-    var numberTwo = getNumberTwo();
-
-    var product = numberOne * numberTwo;
-    document.getElementById("total").innerHTML = 'Total: ' + product;
-}
-
-function divideNumbers() {
-    var numberOne = getNumberOne();
-    var numberTwo = getNumberTwo();
-
-    var quotient = numberOne / numberTwo;
-    document.getElementById("total").innerHTML = 'Total: ' + quotient;
 }
 
 function getNumberOne() {
